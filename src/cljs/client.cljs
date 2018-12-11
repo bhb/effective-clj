b7cff4340de1257a1f9475d2b4b2b7a46cda78c4
Ben Brinckerhoff <ben@bbrinck.com>
2018-12-10 19:58:32 -0700

    Adds example of pure vs transparent functions

--- src/cljs/client.cljs
+++ src/cljs/client.cljs
@@ -1,1 +1,43 @@
 (ns client)
+
+(js/alert "hi")
+
+;; Effect-ive Clojure
+
+;; Effects
+
+;; Effect$$$
+
+;; Pure function
+
+;; Pure, but opaque
+
+(defn foo [key-fn]
+  (juxt key-fn identity))
+
+;; Easy to test? Yes
+;; Easy to iterate upon? No
+;; Easy to reason about? No
+(foo first)
+(foo :a)
+
+;; Pure is good, but *transparent* is even better
+
+(defn baz [m1 m2]
+  (-> m1
+      (into m2)
+      (assoc :count (+ (count m1)
+                       (count m2)))))
+
+(baz {} {})
+
+;; Example - getting stock price via symbol or ticker
+
+
+
+
+
+
+
+
+
