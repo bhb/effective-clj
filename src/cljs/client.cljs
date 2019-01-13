b3b37d4e30870d89a4ae0ef6e41c4d6763130834
Ben Brinckerhoff <ben@bbrinck.com>
2019-01-13 13:46:38 -0700

    Use promises to clean up readability

--- src/cljs/client.cljs
+++ src/cljs/client.cljs
@@ -545,7 +545,75 @@
 
 ;; use tools to improve readability
 
+(defn price-reqs7 [dates-str symbol]
+  (price-reqs (reader/read-string dates-str) symbol))
 
+(defn dates-req7 [looked-up-symbol provided-symbol]
+  {:url "http://localhost:3333/dates-available"
+   :params {:symbol (or looked-up-symbol provided-symbol)}})
+
+(defn symbol-req7 [name]
+  {:url "http://localhost:3333/symbol"
+   :params {:name name}})
+
+(defn get!7
+  ([req cb]
+   (get! req cb cb))
+  ([req cb eb]
+   (http/GET (:url req)
+             (assoc req
+                    :handler cb
+                    :error-handler eb))))
+
+(defn get+7 [req]
+  (let [{:keys [url params]} req]
+    (js/Promise. (fn [resolve reject]
+                   (http/GET url
+                             {:params params
+                              :handler resolve
+                              :error-handler reject})))))
+
+;; TODO - rename
+(defn get+7-both-paths [req]
+  (let [{:keys [url params]} req]
+    (js/Promise. (fn [resolve reject]
+                   (http/GET url
+                             {:params params
+                              :handler resolve
+                              :error-handler resolve})))))
+
+(defn response->sym7 [response]
+  (if (and (map? response) (= 404 (:status response)))
+    nil
+    response))
+
+(defn get-low-price7 [name symbol cb eb]
+  (-> (get+7-both-paths (symbol-req name))
+      (.then (fn [response]
+               (let [looked-up-symbol (response->sym response)
+                     req (dates-req7 looked-up-symbol symbol)]
+                 (-> (js/Promise.all [(get+7 req) (js/Promise.resolve req)])))))
+      (.then (fn [[dates req]]
+               (let [reqs (price-reqs7 dates (-> req :params :symbol))
+                     ps (map get+7 reqs)]
+                 (js/Promise.all ps))))
+      (.then #(apply min %))
+      (.then cb)
+      (.catch eb)))
+
+(comment
+  (get-low-price7 "Google" nil ok! fail!)
+  (get-low-price7 nil "GOOGL" ok! fail!)
+  (get-low-price7 nil nil ok! fail!)
+  )
+
+
+(comment
+  (symbol-req nil)
+  (price-reqs7 "[\"2018-12-01\"]" "GOOGL")
+  (get-low-price7 "Google" nil ok! fail!)
+  (get-low-price7 nil "GOOGL" ok! fail!)
+  )
 
 ;; 1. colocate as much as possible (reasoning) (Understanding comes from introspection and readability)
 ;; 2. use tools to improve readability
